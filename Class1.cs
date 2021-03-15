adf00f88a30ab2958ba125f0d129995d2ed6321c
Chris Reddington <791642+chrisreddington@users.noreply.github.com>
2021-03-15 14:36:57 +0000

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -4,6 +4,9 @@ namespace nuget_repro
 {
     public class Class1
     {
-        Console.WriteLine("Example package");
+        public void Log(string text)
+        {
+            Console.WriteLine(text);
+        }
     }
 }
